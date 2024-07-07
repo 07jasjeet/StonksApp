fb1f5423859e37acca6fd153af16a755a9166732
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2024-07-08 00:37:31 +0530

    Added material icons extended library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,6 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     kapt(libs.androidx.hilt.compiler)
 
-    implementation(libs.material)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -83,6 +81,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
